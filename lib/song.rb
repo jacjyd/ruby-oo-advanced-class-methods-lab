a8854e7e6c3a8e73f086c740dc4da8b44257ccd8
Diana Liao <dijali@gmail.com>
2021-02-25 19:59:11 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    Song.all << song
+    song
+  end 
+
+  def self.new_by_name (name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name (name)
+    song = Song.new
+    song.name = name
+    Song.all << song
+    song
+  end 
+
+  def self.find_by_name (name)
+    Song.all.find{|song| song.name == name}
+  end 
+
+  def self.find_or_create_by_name (name)
+    if !Song.all.map{|song| song.name}.include?(name)
+      new_song = Song.create_by_name(name)
+    end
+    Song.find_by_name (name)
+  end
+
+  def self.alphabetical
+    Song.all.sort_by{|song| song.name}
+  end 
+
+  def self.new_from_filename (filename)
+    song_info = filename.split(/[-.]/).map{|n| n.strip}
+    new_song = Song.create_by_name(song_info[1])
+    new_song.artist_name = song_info[0]
+
+    new_song
+  end
+
+  def self.create_from_filename (filename)
+    song_info = filename.split(/[-.]/).map{|n| n.strip}
+    new_song = Song.create_by_name(song_info[1])
+    new_song.artist_name = song_info[0]
+
+    new_song.save
+  end
+
+  def self.destroy_all
+    Song.all.clear
+  end
+
 end
